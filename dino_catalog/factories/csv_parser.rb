2c1efda1aee77b57f53acfc5cc2e2bc0f048daf5
Franco Reyes <freyes@enova.com>
2014-07-04 11:44:48 -0500

    Refactor CSVParser
    A lot of unnecessary code in csv parser.

--- dino_catalog/factories/csv_parser.rb
+++ dino_catalog/factories/csv_parser.rb
@@ -10,37 +10,12 @@ class CSVParser
   end
 
   def to_arr_of_hash
-    head = Array.new
-    data = Array.new
-
-    res_hash = Array.new
-    i = 0
-
-    CSV.foreach(@path) do |row|
-      head.push(row) if i == 0
-
-      if i > 0
-        data.push(row)
-
-        temp_hold = Array.new
-        head.last.length.times do |j|
-          temp_hold.push(head.last[j])
-          temp_hold.push(data.last[j])
-        end
-
-        res_hash.push(Hash[*temp_hold])
-
-      end
-      i += 1
-    end
-
-    res_hash
-
+    contents = File.read(@path)
+    CSV.parse(contents,headers: :true, header_converters: :symbol).map(&:to_hash)
   end
 
   def to_json
     hash_ar = to_arr_of_hash
-
     hash_ar.to_json
   end
 end
