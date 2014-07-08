71faaaf9eee93f847965b5a4c8bf42bd8d5bc7bb
Franco Reyes <freyes@enova.com>
2014-07-08 12:30:26 -0500

    Refactor the dinofetcher
    Removed one loop layer that was just plain making things harder to deal
    with.

--- dino_catalog/factories/dino_fetcher.rb
+++ dino_catalog/factories/dino_fetcher.rb
@@ -10,45 +10,75 @@ class DinoFetcher
 
   def get_dinosaurs(options)
     dino_ar = Array.new
+    opt_hash = options.to_h
+
     @file_paths.each do |file|
       CSVParser.new(file).to_arr_of_hash.each do |elem|
         dino = Dinosaur.new(elem)
-        match_flag = true
 
-        options.each_pair do |struct_key, value|
-
-          match_flag = false if struct_key == :name && dino.name != value
-
-          if struct_key == :diet
-            match_flag = false if dino.carnivore? == true && value == 'herbivore'
-            match_flag = false if dino.carnivore? == false && value == 'carnivore'
-          end
-
-          match_flag = false if struct_key == :period && dino.period != value
-
-          if struct_key == :walk
-            match_flag = false if dino.bipedal? == true && value == 'quadriped'
-            match_flag = false if dino.quadriped? == false && value == 'biped'
-          end
-
-          if struct_key == :size
-            if value.include? "+"
-              value[0] = ''
-              match_flag = false if dino.weight < value
-            elsif value.include? "-"
-              value[0] = ''
-              match_flag = false if dino.weight > value
-            else
-              match_flag = false if dino.weight != value
-            end
-          end
-
+        if opt_hash.has_key?(:name)
+          next if !name_match_crit?(dino, opt_hash[:name])
         end
-        dino_ar.push(dino) if match_flag
+
+        if opt_hash.has_key?(:diet)
+          next if !diet_match_crit?(dino, opt_hash[:diet])
+        end
+
+        if opt_hash.has_key?(:period)
+          next if !period_match_crit?(dino, opt_hash[:period])
+        end
+
+        if opt_hash.has_key?(:walking_type)
+          next if !walk_match_crit?(dino, opt_hash[:walking_type])
+        end
+
+        if opt_hash.has_key?(:weight)
+          next if !weight_match_crit?(dino, opt_hash[:weight])
+        end
+
+        dino_ar.push(dino)
       end
     end
 
     dino_ar
+  end
+
+  def name_match_crit?(dino,value)
+    dino.name == value ? true : false
+  end
+
+  def diet_match_crit?(dino,value)
+    crit_match = false
+    crit_match = true if value.to_s == 'carnivore' && dino.carnivore?
+    crit_match = true if value.to_s == 'herbivore' && !dino.carnivore?
+    crit_match
+  end
+
+  def period_match_crit?(dino,value)
+    dino.period?(value)
+  end
+
+  def walk_match_crit?(dino,value)
+    crit_match = false
+    crit_match = true if value.to_s == "biped" && dino.biped?
+    crit_match = true if value.to_s == 'quadriped' && dino.quadriped?
+    crit_match
+  end
+
+  def weight_match_crit?(dino,value)
+    crit_match = false
+
+    if dino.weight != nil
+      if value.include? "+"
+        crit_match = true if dino.weight.to_i > value[1..-1].to_i
+      elsif value.include? "-"
+        crit_match = true if dino.weight.to_i < value[1..-1].to_i
+      else
+        crit_match = true if dino.weight.to_i == value.to_i
+      end
+    end
+
+    crit_match
   end
 
 end
