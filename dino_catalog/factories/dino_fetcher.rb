5fbb04597f6197bc8427f201289be9b559760525
Franco Reyes <freyes@enova.com>
2014-07-09 11:30:26 -0500

    Refactor DinoFetcher for readability
    Variable names have been renamed so that their role is easier to
    understand as well as the creation of a new method which specifically
    handles the dinosaur filtering

--- dino_catalog/factories/dino_fetcher.rb
+++ dino_catalog/factories/dino_fetcher.rb
@@ -10,31 +10,12 @@ class DinoFetcher
 
   def get_dinosaurs(options)
     dino_ar = Array.new
-    opt_hash = options.to_h
 
     @file_paths.each do |file|
       CSVParser.new(file).to_arr_of_hash.each do |elem|
         dino = Dinosaur.new(elem)
 
-        if opt_hash.has_key?(:name)
-          next if !name_match_crit?(dino, opt_hash[:name])
-        end
-
-        if opt_hash.has_key?(:diet)
-          next if !diet_match_crit?(dino, opt_hash[:diet])
-        end
-
-        if opt_hash.has_key?(:period)
-          next if !period_match_crit?(dino, opt_hash[:period])
-        end
-
-        if opt_hash.has_key?(:walking_type)
-          next if !walk_match_crit?(dino, opt_hash[:walking_type])
-        end
-
-        if opt_hash.has_key?(:weight)
-          next if !weight_match_crit?(dino, opt_hash[:weight])
-        end
+        next if !dino_match_filter?(dino,options.to_h)
 
         dino_ar.push(dino)
       end
@@ -43,42 +24,66 @@ class DinoFetcher
     dino_ar
   end
 
-  def name_match_crit?(dino, value)
+  def dino_match_filter?(dino, opt_hash)
+    if opt_hash.has_key?(:name)
+      return if !name_match_filter?(dino, opt_hash[:name])
+    end
+
+    if opt_hash.has_key?(:diet)
+      return if !diet_match_filter?(dino, opt_hash[:diet])
+    end
+
+    if opt_hash.has_key?(:period)
+      return if !period_match_filter?(dino, opt_hash[:period])
+    end
+
+    if opt_hash.has_key?(:walking_type)
+      return if !walk_match_filter?(dino, opt_hash[:walking_type])
+    end
+
+    if opt_hash.has_key?(:weight)
+      return if !weight_match_filter?(dino, opt_hash[:weight])
+    end
+
+    true
+  end
+
+  def name_match_filter?(dino, value)
     dino.name == value ? true : false
   end
 
-  def diet_match_crit?(dino, value)
-    crit_match = false
-    crit_match = true if value.to_s == 'carnivore' && dino.carnivore?
-    crit_match = true if value.to_s == 'herbivore' && !dino.carnivore?
-    crit_match
+  def diet_match_filter?(dino, value)
+    filter_match = false
+    filter_match = true if value.to_s == 'carnivore' && dino.carnivore?
+    filter_match = true if value.to_s == 'herbivore' && !dino.carnivore?
+    filter_match
   end
 
-  def period_match_crit?(dino, value)
+  def period_match_filter?(dino, value)
     dino.period?(value)
   end
 
-  def walk_match_crit?(dino, value)
-    crit_match = false
-    crit_match = true if value.to_s == "biped" && dino.biped?
-    crit_match = true if value.to_s == 'quadriped' && dino.quadriped?
-    crit_match
+  def walk_match_filter?(dino, value)
+    filter_match = false
+    filter_match = true if value.to_s == "biped" && dino.biped?
+    filter_match = true if value.to_s == 'quadriped' && dino.quadriped?
+    filter_match
   end
 
-  def weight_match_crit?(dino, value)
-    crit_match = false
+  def weight_match_filter?(dino, value)
+    filter_match = false
 
     if dino.weight != nil
       if value.include? "+"
-        crit_match = true if dino.weight.to_i > value[1..-1].to_i
+        filter_match = true if dino.weight.to_i > value[1..-1].to_i
       elsif value.include? "-"
-        crit_match = true if dino.weight.to_i < value[1..-1].to_i
+        filter_match = true if dino.weight.to_i < value[1..-1].to_i
       else
-        crit_match = true if dino.weight.to_i == value.to_i
+        filter_match = true if dino.weight.to_i == value.to_i
       end
     end
 
-    crit_match
+    filter_match
   end
 end
 
