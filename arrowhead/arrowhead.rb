9472a505997a518c63e802c59d590c77d9d03777
Franco Reyes <freyes@enova.com>
2014-07-09 11:15:36 -0500

    Arrowhead exercise
    Refactor code to keep error messages close to their test conditions

--- arrowhead/arrowhead.rb
+++ arrowhead/arrowhead.rb
@@ -16,23 +16,26 @@ class Arrowhead
     },
   }
 
-  # FIXME: I don't have time to deal with this.
   def self.classify(region, shape)
+    get_arrowhead(get_shapes(region), shape)
+  end
+
+  def self.get_shapes(region)
     if CLASSIFICATIONS.include? region
-      shapes = CLASSIFICATIONS[region]
-      if shapes.include? shape
-        arrowhead = shapes[shape]
-        puts "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
-      else
-        raise "Unknown shape value. Are you sure you know what you're talking about?"
-      end
+      CLASSIFICATIONS[region]
     else
       raise "Unknown region, please provide a valid region."
     end
   end
 
+  def self.get_arrowhead(shapes, shape)
+    if shapes.include? shape
+      puts "You have a(n) '#{shapes[shape]}' arrowhead. Probably priceless."
+    else
+      raise "Unknown shape value. Are you sure you know what you're talking about?"
+    end
+  end
 end
 
+puts Arrowhead.classify(:northern_plains, :bifurcated)
 
-
-puts Arrowhead::classify(:northern_plains, :bifurcated)
