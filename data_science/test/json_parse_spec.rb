52c0af548cf896d49aa3ac930a055e6e3b063ebe
Franco Reyes <freyes@enova.com>
2014-07-14 14:10:52 -0500

    Updated JSON Test.
    JSON test data has been updated to have ten entries. The test for the
    JSON parser has been updated

--- data_science/test/json_parse_spec.rb
+++ data_science/test/json_parse_spec.rb
@@ -3,6 +3,17 @@ describe "JSON Parser" do
     file = "/Users/freyes/development_zone/level_up_exercises/data_science/data/test_data.json"
     json_parser = JSONParser.new(file)
     parse = json_parser.parse
-    parse.should equal([{:date => Date.new(2014,03,20), :cohort => "B", :result => 0}])
+    parse.should equal([
+                       {:date => Date.new(2014,03,20), :cohort => "B", :result => 0},
+                       {:date => Date.new(2014,03,20), :cohort => "B", :result => 0},
+                       {:date => Date.new(2014,03,20), :cohort => "B", :result => 0},
+                       {:date => Date.new(2014,03,20), :cohort => "B", :result => 0},
+                       {:date => Date.new(2014,03,20), :cohort => "B", :result => 0},
+                       {:date => Date.new(2014,03,20), :cohort => "A", :result => 0},
+                       {:date => Date.new(2014,03,20), :cohort => "A", :result => 0},
+                       {:date => Date.new(2014,03,20), :cohort => "B", :result => 0},
+                       {:date => Date.new(2014,03,20), :cohort => "B", :result => 0},
+                       {:date => Date.new(2014,03,20), :cohort => "B", :result => 1}
+    ])
   end
 end
