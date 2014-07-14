353508b7dc8326ad8a69ea1036d63e93bbc72b16
Franco Reyes <freyes@enova.com>
2014-07-14 12:20:12 -0500

    JSON parse test added
    JSON parse test now will test against array of hash structure and a
    test JSON file has been created in the data directory.

--- data_science/test/json_parse_spec.rb
+++ data_science/test/json_parse_spec.rb
@@ -1,3 +1,8 @@
-class RSpecJsonParse
-
+describe "JSON Parser" do
+  it "Should parse JSON" do
+    file = "/Users/freyes/development_zone/level_up_exercises/data_science/data/test_data.json"
+    json_parser = JSONParser.new(file)
+    parse = json_parser.parse
+    parse.should equal([{:date => Date.new(2014,03,20), :cohort => "B", :result => 0}])
+  end
 end
