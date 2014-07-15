0d64ba691fd78ad4b09883a13e733cb9706d3125
Franco Reyes <freyes@enova.com>
2014-07-15 16:07:29 -0500

    Robot Name: Short method exercise
    Small methods created for the robot name exercise.

--- short_methods/robot_name.rb
+++ short_methods/robot_name.rb
@@ -1,26 +1,39 @@
+require 'pp'
 class NameCollisionError < RuntimeError; end;
 
 class Robot
   attr_accessor :name
-
   @@registry
 
   def initialize(args = {})
     @@registry ||= []
-    @name_generator = args[:name_generator]
 
+    @name = generate_name(args[:name_generator])
+    raise NameCollisionError, 'There was a problem generating the robot name!' unless name_valid?(@name)
 
-    if @name_generator
-      @name = @name_generator.call
-    else
-      generate_char = -> { ('A'..'Z').to_a.sample }
-      generate_num = -> { rand(10) }
-
-      @name = "#{generate_char.call}#{generate_char.call}#{generate_num.call}#{generate_num.call}#{generate_num.call}"
-    end
-
-    raise NameCollisionError, 'There was a problem generating the robot name!' if !(name =~ /[[:alpha:]]{2}[[:digit:]]{3}/) || @@registry.include?(name)
     @@registry << @name
+  end
+
+  private
+
+  def generate_name(generator)
+    if generator
+      generator.call
+    else
+      "#{generate_char}#{generate_char}#{generate_num}#{generate_num}#{generate_num}"
+    end
+  end
+
+  def name_valid?(name)
+    (name =~ /[[:alpha:]]{2}[[:digit:]]{3}/) && !@@registry.include?(name)
+  end
+
+  def generate_char
+    ('A'..'Z').to_a.sample
+  end
+
+  def generate_num
+    rand(10)
   end
 
 end
@@ -29,6 +42,8 @@ robot = Robot.new
 puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
 
 # Errors!
-# generator = -> { 'AA111' }
-# Robot.new(name_generator: generator)
-# Robot.new(name_generator: generator)
+generator = -> { 'AA111' }
+puts "My other robo is #{Robot.new(name_generator: generator).name}"
+generator = -> { 'A1111' }
+puts "My third robo is #{Robot.new(name_generator: generator).name}"
+#
