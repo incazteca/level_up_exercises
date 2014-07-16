6eab59e43a6265abc287c69df67af796702ab14e
Franco Reyes <freyes@enova.com>
2014-07-16 16:39:41 -0500

    JSON parse test updated
    Parsing doesn't have to mess with data just place it in array of hash.
    For that reason removing creation of Date objects from the test. Also
    updated path of test JSON.

--- data_science/test/json_parse_spec.rb
+++ data_science/test/json_parse_spec.rb
@@ -1,27 +1,25 @@
 require_relative('../src/json_parser.rb')
 
 describe JSONParser do
-  let(:file) { File.join(__FILE__, "data", "test_data.json") }
+  let(:file) { File.join('.', "data", "test_data.json") }
   let(:json_parser) { JSONParser.new(file) }
 
   it "store file path" do
     expect(json_parser.path).to equal(file)
   end
 
-
-  it "Should parse JSON to an array of hashes" do
-    expect(json_parser.to_arr_of_hash).to equal(
-    [
-      {:date => Date.new(2014,03,20), :cohort => "B", :result => 0},
-      {:date => Date.new(2014,03,20), :cohort => "B", :result => 0},
-      {:date => Date.new(2014,03,20), :cohort => "B", :result => 0},
-      {:date => Date.new(2014,03,20), :cohort => "B", :result => 0},
-      {:date => Date.new(2014,03,20), :cohort => "B", :result => 0},
-      {:date => Date.new(2014,03,20), :cohort => "A", :result => 0},
-      {:date => Date.new(2014,03,20), :cohort => "A", :result => 0},
-      {:date => Date.new(2014,03,20), :cohort => "B", :result => 0},
-      {:date => Date.new(2014,03,20), :cohort => "B", :result => 0},
-      {:date => Date.new(2014,03,20), :cohort => "B", :result => 1}
+  it "parses JSON to an array of hashes" do
+    expect(json_parser.to_arr_of_hash).to eq([
+      {:date => "2014-03-20", :cohort => "B", :result => 0},
+      {:date => "2014-03-20", :cohort => "B", :result => 0},
+      {:date => "2014-03-20", :cohort => "B", :result => 0},
+      {:date => "2014-03-20", :cohort => "B", :result => 0},
+      {:date => "2014-03-20", :cohort => "B", :result => 0},
+      {:date => "2014-03-20", :cohort => "A", :result => 0},
+      {:date => "2014-03-20", :cohort => "A", :result => 0},
+      {:date => "2014-03-20", :cohort => "B", :result => 0},
+      {:date => "2014-03-20", :cohort => "B", :result => 0},
+      {:date => "2014-03-20", :cohort => "B", :result => 1}
     ])
   end
 end
