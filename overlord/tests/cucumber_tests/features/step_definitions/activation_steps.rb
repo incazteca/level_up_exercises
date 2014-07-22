7ec9e93bfc87f5f7bd5f7c0b7573e72b3e3b33b0
Franco Reyes <freyes@enova.com>
2014-07-22 16:47:01 -0500

    We should expect the buttons to be capitalized
    Test updated because the button shop only sells buttons with capitalized
    words.

--- overlord/tests/cucumber_tests/features/step_definitions/activation_steps.rb
+++ overlord/tests/cucumber_tests/features/step_definitions/activation_steps.rb
@@ -6,15 +6,15 @@ Capybara.app = Sinatra::Application.new
 Capybara.javascript_driver = :webkit
 
 When(/^I turn the bomb on$/) do
-  click_button 'On/Off'
+  click_button 'ON/OFF'
 end
 
 When(/^I activate it$/) do
-  click_button 'Activate'
+  click_button 'ACTIVATE'
   click_button '1'
   click_button '2'
   click_button '3'
   click_button '4'
-  click_button 'Enter'
+  click_button 'ENTER'
 end
 
--- overlord/tests/cucumber_tests/features/step_definitions/deactivation_steps.rb
+++ overlord/tests/cucumber_tests/features/step_definitions/deactivation_steps.rb
@@ -6,19 +6,19 @@ Capybara.app = Sinatra::Application.new
 Capybara.javascript_driver = :webkit
 
 Given(/^an activated bomb$/) do
-  click_button 'Activate'
+  click_button 'ACTIVATE'
   click_button '1'
   click_button '2'
   click_button '3'
   click_button '4'
-  click_button 'Enter'
+  click_button 'ENTER'
 end
 
 When(/^I deactivate it$/) do
-  click_button 'Deactivate'
+  click_button 'DEACTIVATE'
   click_button '0'
   click_button '0'
   click_button '0'
   click_button '0'
-  click_button 'Enter'
+  click_button 'ENTER'
 end
